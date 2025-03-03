bb01111a16cb3abc7e16e0ce8650ed2dc4127e70
Mayank Mishra <maymishraank@gmail.com>
2025-03-03 14:12:57 +0530

    astronvim configuration

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+        },
+        t = {
+          ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+        },
+      },
+    },
+  },
+}
