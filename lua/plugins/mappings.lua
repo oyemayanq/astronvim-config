628f43eaa645c192a687706bae498b5416a8a625
Mayank Mishra <maymishraank@gmail.com>
2025-03-03 18:43:57 +0530

    mapped numpad enter to open folder

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,6 +5,7 @@ return {
       mappings = {
         n = {
           ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+          ["<kEnter>"] = { "<CR>", desc = "Numpad Enter" },
         },
         t = {
           ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
