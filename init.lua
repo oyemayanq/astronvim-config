6ac0862980cbc24a2069cce23554bc5cf6dbb5b9
Mayank Mishra <maymishraank@gmail.com>
2025-03-04 13:49:30 +0530

    theme and references mapping

--- init.lua
+++ init.lua
@@ -17,3 +17,10 @@ end
 
 require "lazy_setup"
 require "polish"
+require("telescope").setup {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules",
+    },
+  },
+}
--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -6,6 +6,8 @@ return {
         n = {
           ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
           ["<kEnter>"] = { "<CR>", desc = "Numpad Enter" },
+          ["<leader>tc"] = { "<cmd>Telescope colorscheme<cr>", desc = "Change Colorscheme" },
+          ["gr"] = { "<cmd>Telescope lsp_references<cr>", desc = "Find References" },
         },
         t = {
           ["<C-`>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
